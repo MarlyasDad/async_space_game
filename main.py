102963158f00f689911f01d1f953f2d7b726ae71
MarlyasDad <yudaev.alex@gmail.com>
2019-04-21 01:40:13 +0300

    Add event loop and class Sleep

--- main.py
+++ main.py
@@ -1,48 +1,72 @@
-import time
 import asyncio
 import curses
 
+TIC_TIMEOUT = 0.1
 
-async def blink(canvas, row, column, symbol='*'):
+
+class EventLoopCommand:
+
+    def __await__(self):
+        return (yield self)
+
+
+class Sleep(EventLoopCommand):
+
+    def __init__(self, seconds):
+        self.seconds = seconds
+
+
+def convert_seconds_to_iterations(seconds):
+    return seconds * 1000000
+
+
+async def refresh(canvas, seconds):
+    while True:
+        canvas.refresh()
+        await Sleep(seconds)
+
+
+async def blink_the_star(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        await asyncio.sleep(0)
+        await Sleep(2)
 
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
+        await Sleep(0.3)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        await asyncio.sleep(0)
+        await Sleep(0.5)
 
         canvas.addstr(row, column, symbol)
-await asyncio.sleep(0)
+        await Sleep(0.3)
 
 
 def draw(canvas):
-    row, column = (1, 10)
     canvas.border(0)
     canvas.nodelay(True)
-    while True:
-        # canvas.addstr(row, column, 'Hello, World!')
-        canvas.addstr(row, column, '*', curses.A_DIM)
-        canvas.refresh()
-        time.sleep(2)
-        canvas.addstr(row, column, '*')
-        canvas.refresh()
-        time.sleep(0.3)
-        canvas.addstr(row, column, '*', curses.A_BOLD)
-        canvas.refresh()
-        time.sleep(0.5)
-        canvas.addstr(row, column, '*')
-        canvas.refresh()
-        time.sleep(0.3)
-        key = canvas.getkey()
-        if key == "q":
-            exit()
+
+    coroutines = {
+        refresh(canvas, TIC_TIMEOUT): 0,
+        blink_the_star(canvas, row=1, column=5, symbol=':'): 0,
+        blink_the_star(canvas, row=1, column=7): 0,
+        blink_the_star(canvas, row=1, column=9): 0,
+    }
+
+    while coroutines:
+        for coroutine in coroutines:
+            try:
+                if coroutines[coroutine] <= 0:
+                    timeout = coroutine.send(None).seconds
+                    ticks_to_sleep = convert_seconds_to_iterations(timeout)
+                    coroutines[coroutine] = ticks_to_sleep
+                coroutines[coroutine] -= 1
+            except StopIteration:
+                del coroutines[coroutine]
 
 
 if __name__ == '__main__':
     curses.update_lines_cols()
-    curses.wrapper(draw)
+    curses.initscr()
     curses.curs_set(False)
+    curses.wrapper(draw)
     input('Нажмите любую клавишу для выхода...')
